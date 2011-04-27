0e5c118e768ac29c935061ca19c45053606416be
Andrey Misan <andrei.misan@gmail.com>
2011-04-27 22:04:05 +0300

    some updates

--- src/iplib/app/views.py
+++ src/iplib/app/views.py
@@ -1,21 +1,23 @@
 ﻿# -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
-# from django.template import Context
+# from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse, Http404
 from iplib.app.models import ComponentDefinition, VersionedComponent, Category
 
 
 def home(request):
-    return render_to_response('home.html')
+        return render_to_response('home.html')
     
 def all_components(request):
+    
     html = "все компоненты"
     return HttpResponse(html)
     
 def all_categories(request):
     html = "Все Категории"
     return HttpResponse(html)
-    
+
 def component(request, component_id):
     html = "Какой-то компонент"
     return HttpResponse(html)
